b3ee3e462dea7bf200790495f00479e26bf0dbd2
Laurens Van Houtven (lvh) <_@lvh.io>
2023-02-28 12:01:39 -0600

    WIP

--- src/com/latacora/signcryption_demo.clj
+++ src/com/latacora/signcryption_demo.clj
@@ -1,12 +1,19 @@
 (ns com.latacora.signcryption-demo
   (:require
-   #_[caesium.crypto.aead :as aead]
-   #_[caesium.crypto.sign :as sign]
-   [caesium.util :as cutil]))
+   [caesium.crypto.aead :as aead]
+   [caesium.crypto.sign :as sign]
+   [caesium.util :as cutil]
+   [caesium.byte-bufs :as cbb]))
+
+(comment
+  (System/getProperty "java.library.path")
+  (System/setProperty "java.library.path" (str (System/getProperty "java.library.path") ":/opt/homebrew/lib")))
 
 (def version "N1")
 
-;; # Topic keys
+;; # Symmetric encryption
+
+;; ## Test key generation
 
 (defn symmetric-test-key
   [topic n]
@@ -14,18 +21,37 @@
         key-bytes (concat topic-bytes (repeat n))]
     (->> key-bytes (take 32) byte-array)))
 
-(comment
-  (seq (symmetric-test-key :a 1))
-  #_(97 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1)
-
-  (seq (symmetric-test-key :b 2))
-  #_(98 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2))
-
 (def topic-a-key-1 (symmetric-test-key \a 1))
 (def topic-a-key-2 (symmetric-test-key \a 2))
 (def topic-b-key-1 (symmetric-test-key \b 1))
 (def topic-b-key-2 (symmetric-test-key \b 2))
 
-;; # Signing identities
+(comment
+  (cutil/hexify (symmetric-test-key :a 1))
+  "6101010101010101010101010101010101010101010101010101010101010101"
+
+  (cutil/hexify (symmetric-test-key :b 2))
+  "6202020202020202020202020202020202020202020202020202020202020202")
+
+;; # Signing
+
+;; ## Serialization utilities
+
+(defn serialize-signing-key
+  [key-pair]
+  (let [{:keys [public secret]} key-pair]
+    (map cutil/hexify [public secret])))
+
+(defn deserialize-signing-key
+  [serialized]
+  (->> serialized (map cutil/unhexify) (zipmap [:oublic :secret])))
+
+;; ## Producer identities
+
+(def producer-1-keypair (sign/keypair! "producer-1"))
+(def producer-2-keypair (sign/keypair! "producer-2"))
 
 ;; TKTK
+
+(defn send
+  [])
